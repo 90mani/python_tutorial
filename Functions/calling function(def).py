4d3862b019d812ff3ad5e6bda8f7a20361688bee
90mani <manikandan.c@sankitsolutions.com>
2023-07-12 20:44:07 +0800

    calling function

---
+++ Functions/calling function(def).py
@@ -0,0 +1,29 @@
+def add ():
+    print("addition:")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a+b)
+
+def sub ():
+    print("subtraction:")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a-b)
+
+def mul ():
+    print("multiplication:")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a*b)
+
+
+def div ():
+    print("division:")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a/b)
+
+add()   
+sub() 
+mul()
+div()
