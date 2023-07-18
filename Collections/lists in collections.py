a8b1a2ea2b78cc71dfae888bdff3deecbd8e1203
90mani <manikandan.c@sankitsolutions.com>
2023-07-18 12:16:19 +0530

    collection list model

---
+++ Collections/lists in collections.py
@@ -0,0 +1,36 @@
+#collections (lists)
+
+#add number are variables in end of the line
+
+a=[1,2,3,4,5,6]
+print(a)
+a.append(54)
+a.append(True)
+a.append("emc")
+print(a)
+
+
+#print specific postion 
+
+b=[1,2,3,4,5,6]
+print(b[0])
+
+
+#insert specific number are variable in specific position
+
+c=[1,2,3,4,5,6]
+c.insert(0,55)
+print(c)
+
+#replace position number 
+
+d=[1,2,3,4,5,6]
+d[0]=55
+print(d)
+
+#remove particular number in line 
+
+e=[1,2,3,4,5,6]
+e.pop(5)
+print(e)
+
