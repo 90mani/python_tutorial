a7746ffc4be5bc1ac231098fc3f247e22f2d7cbb
Chinnasamy, Manikandan <Manikandan.Chinnasamy@nielseniq.com>
2023-07-28 14:14:16 +0530

    Added code for data base with flask

--- flask/student.py
+++ flask/student.py
@@ -1,6 +1,7 @@
 # Using flask to make an api
 # import necessary libraries and functions
 from flask import Flask, jsonify, request
+import mysql.connector
 
 
 app = Flask(__name__)
@@ -9,6 +10,15 @@ app = Flask(__name__)
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
+mydb = mysql.connector.connect(
+    host="sql6.freesqldatabase.com",
+    user="sql6635849",
+    password="AyPYNItaJp",
+    database="sql6635849",
+)
+mycursor = mydb.cursor()
+
+
 @app.route("/", methods=["GET"])
 def home():
     if request.method == "GET":
@@ -18,8 +28,11 @@ def home():
 
 @app.route("/student", methods=["GET"])
 def getstudent():
-    data = [{"id": 1001, "name": "kalai", "mark": 65, "grade": "A+"}]
-    return jsonify({"data": data})
+    mycursor.execute("SELECT * from employee where id =11111111111")
+    myresult = mycursor.fetchall()
+    return jsonify({"data": myresult})
+    # data = [{"id": 1001, "name": "kalai", "mark": 65, "grade": "A+"}]
+    # return jsonify({"data": data})
 
 
 # @app.route("/student/<int:id>", methods=["GET"])
