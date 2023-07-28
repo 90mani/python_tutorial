73ddb7dccb17112713caa395aed91c128fb01cd9
Chinnasamy, Manikandan <Manikandan.Chinnasamy@nielseniq.com>
2023-07-28 13:17:37 +0530

    fixed object issue

--- REST API/Using flask to make an api 28.py
+++ REST API/Using flask to make an api 28.py
@@ -10,57 +10,47 @@ app = Flask(__name__)
 # returns the data that we send when we use POST.
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
-	if(request.method == 'GET'):
+    if request.method == "GET":
+        data = "server starting"
+        return jsonify({"data": data})
 
-		data = "server starting"
-		return jsonify({'data': data})
-	
 
-@app.route('/product', methods = ['GET', 'POST'])
+@app.route("/product", methods=["GET", "POST"])
 def product():
-	if(request.method == 'GET'):
-
-		data = {"itemName", "Price", "Quantity"}
-		return jsonify({'data': data})
-
+    if request.method == "GET":
+        data = {"itemName": "Rice", "Price": 1200, "Quantity": "25kg"}
+        return jsonify({"data": data})
 
 
-@app.route('/list', methods = ['GET', 'POST'])
+@app.route("/list", methods=["GET", "POST"])
 def list():
-	if(request.method == 'GET'):
-
-		data = [{"itemName":"Rice", "Price":1200, "Quantity":"25kg"},
-	  {"itemName":"Uruttu dhal", "Price":60, "Quantity":"1kg"},
-	  {"itemName":"Sun flower Oil", "Price":170, "Quantity":"1ltr"},
-			
+    if request.method == "GET":
+        data = [
+            {"itemName": "Rice", "Price": 1200, "Quantity": "25kg"},
+            {"itemName": "Uruttu dhal", "Price": 60, "Quantity": "1kg"},
+            {"itemName": "Sun flower Oil", "Price": 170, "Quantity": "1ltr"},
         ]
-		return jsonify({'data': data})
-	
+        return jsonify({"data": data})
 
 
-@app.route('/isAvailable', methods = ['GET', 'POST'])
+@app.route("/isAvailable", methods=["GET", "POST"])
 def isavilable():
-	if(request.method == 'GET'):
-
-		data = {"message":"yes"}
-		return jsonify({'data': data})    
-
-
+    if request.method == "GET":
+        data = {"message": "yes"}
+        return jsonify({"data": data})
 
 
 # A simple function to calculate the square of a number
 # the number to be squared is sent in the URL when we use GET
 # on the terminal type: curl http://127.0.0.1:5000 / home / 10
 # this returns 100 (square of 10)
-@app.route('/home/<int:num>', methods = ['GET'])
+@app.route("/home/<int:num>", methods=["GET"])
 def disp(num):
-
-	return jsonify({'data': num**2})
+    return jsonify({"data": num**2})
 
 
 # driver function
-if __name__ == '__main__':
-
-	app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
