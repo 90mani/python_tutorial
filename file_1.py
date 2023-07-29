ce9fe1c3ce85d09f2592c6ac15bf18bc9555e666
90mani <139115642+90mani@users.noreply.github.com>
2023-07-29 17:58:52 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import mysql.connector
+
+
+app = Flask(__name__)
+
+
+mydb = mysql.connector.connect(
+    host="sql6.freesqldatabase.com",
+    user="sql6635850",
+    password="WWhxxlLRHb",
+    database="sql6635850",
+)
+mycursor = mydb.cursor()
+
+
+@app.route("/", methods = ['GET'])
+def home():
+	if request.method == 'GET':
+		data = "server starting"
+                 return jsonify({"data": data})       
+
+@app.route("/student", methods=["GET"])
+def getstudent():
+    mycursor.execute("SELECT * from user_details")
+    myresult = mycursor.fetchall()
+    return jsonify({"data": "first_name"})
+
+	
+if __name__ == "__main__":
+    app.run(debug=True)	
+   
