fde6325f69a8231a80bb5262fa80627d985175d1
90mani <manikandan.c@sankitsolutions.com>
2023-07-31 19:50:53 +0530

    basic fill

---
+++ pythone. 1.py
@@ -0,0 +1,21 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone. 1.py
+python basics
+>>> 
+= RESTART: C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone. 1.py
+1.2
+>>> 
+= RESTART: C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone. 1.py
+abc 1.2 xyz
+>>> 
+= RESTART: C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone. 1.py
+hi python
+hello
+>>> 
+= RESTART: C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone. 1.py
+Traceback (most recent call last):
+  File "C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone. 1.py", line 5, in <module>
+    print("Addition:",Add_result)
+NameError: name 'Add_result' is not defined. Did you mean: 'add_result'?
--- tutorial/forLoop/simpleForLoop.py
+++ tutorial/forLoop/simpleForLoop.py
@@ -1,3 +1,15 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x) 
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Welcome/AppData/Local/Programs/Python/Python311/pythone.nandhini.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
