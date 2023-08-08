cb9a00d16032447171008cb444313adb89d83b77
Chinnasamy, Manikandan <Manikandan.Chinnasamy@nielseniq.com>
2023-08-08 13:18:57 +0530

    lastest code

---
+++ tutorial/RESTapi1/decorator.py
@@ -0,0 +1,11 @@
+@gfg_decorator
+def hello_decorator():
+    print("Gfg")
+
+
+"""Above code is equivalent to -
+
+def hello_decorator():
+    print("Gfg")
+    
+hello_decorator = gfg_decorator(hello_decorator)"""
