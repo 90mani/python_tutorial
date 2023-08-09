96a31b35cf2c67aca4a8629db4e3b288a30d6d51
Chinnasamy, Manikandan <Manikandan.Chinnasamy@nielseniq.com>
2023-08-09 13:23:29 +0530

    added code for simple swagger

--- tutorial/RestFulServices/EmployeeServices.py
+++ tutorial/RestFulServices/EmployeeServices.py
@@ -1,22 +1,26 @@
 # Using flask to make an api
 # import necessary libraries and functions
 from flask import Flask, jsonify, request
+from flask_swagger_ui import get_swaggerui_blueprint
+import json
+from flask_cors import CORS
 
 # creating a Flask app
 app = Flask(__name__)
+CORS(app)
 
 
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
-@app.route("/", methods=["GET"])
+@app.route("/api/hello", methods=["GET"])
 def home():
     if request.method == "GET":
         data = "Server started"
         return jsonify({"data": data})
 
 
-@app.route("/employees", methods=["GET"])
+@app.route("/api/employees", methods=["GET"])
 def getEmployee():
     if request.method == "GET":
         data = [
@@ -51,6 +55,21 @@ def getEmployeeAndLeaves():
 @app.route("/home/<int:num>", methods=["GET"])
 def disp(num):
     return jsonify({"data": num**2})
+
+
+# Configure Swagger UI
+SWAGGER_URL = "/swagger"
+API_URL = "http://127.0.0.1:5000/swagger.json"
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Sample API"}
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+@app.route("/swagger.json")
+def swagger():
+    with open("C:\\Nielsen\\swagger.json", "r") as f:
+        return jsonify(json.load(f))
 
 
 # driver function
