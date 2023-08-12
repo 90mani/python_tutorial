79a2ca7c180f4d8daeb946d08fe6cd0e4ef30c53
Chinnasamy, Manikandan <Manikandan.Chinnasamy@nielseniq.com>
2023-08-12 13:59:47 +0530

    added code post api

--- swagger/student_data.py
+++ swagger/student_data.py
@@ -5,15 +5,13 @@ from marshmallow import Schema, fields
 from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec.extension import FlaskApiSpec
 from flask_apispec.views import MethodResource
-from flask_apispec import marshal_with, doc
+from flask_apispec import marshal_with, doc, use_kwargs
 import mysql.connector
-
 
 
 app = Flask(__name__)
 
 api = Api(app)
-
 
 
 app.config.update(
@@ -31,51 +29,55 @@ app.config.update(
 docs = FlaskApiSpec(app)
 
 mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="2003@saran",
-    database="employee_mgnt",
+    host="sql6.freesqldatabase.com",
+    user="sql6637633",
+    password="JlHqEM7Xl5",
+    database="sql6637633",
 )
 mycursor = mydb.cursor()
 
 
+class StudentReuqetBody(Schema):
+    userId = fields.Int(required=True, description="Student id")
+    u_name = fields.String(required=True, description="Student name")
+    passcode = fields.String(required=True, description="Student name")
+    number = fields.Int(required=True, description="Student name")
+    no_experiance = fields.Int(required=False, description="Student name")
+
+
 class Hello(MethodResource, Resource):
-     @doc(description="It's my first get hello api", tags=["Hello API"])
-
-     def get(self):
+    @doc(description="It's my first get hello api", tags=["Hello API"])
+    def get(self):
         return jsonify({"data": "hello world"})
-     
-class student(MethodResource, Resource):
-          
-     @doc(description="get student data", tags=["Welcome"])
 
-     def get(student):
+
+class student(MethodResource, Resource):
+    @doc(description="get student data", tags=["Student"])
+    def get(self):
         if request.method == "GET":
             mycursor.execute("SELECT * from employee")
             myresult = mycursor.fetchall()
             return jsonify({"data": myresult})
-     def post(studentdetails):    
+
+    @doc(description="insert student data", tags=["Student"])
+    @use_kwargs(StudentReuqetBody, location=("json"))
+    def post(self, **kwargs):
         if request.method == "POST":
             requestPayload = request.get_json()
-            print(requestPayload)
-            print(requestPayload["userId"])
+            userId = requestPayload["userId"]
+            name = requestPayload["u_name"]
+            passcode = requestPayload["passcode"]
+            number = requestPayload["number"]
+            no_experiance = requestPayload["no_experiance"]
             sql = "INSERT INTO employee (employee_id,name,password,phone_number,experiance) VALUES (%s,%s,%s,%s,%s)"
-            val = (
-            requestPayload["userId"],
-            requestPayload["u_name"],
-            requestPayload["passcode"],
-            requestPayload["number"],
-            requestPayload["no_experiance"],
-            )
+            val = (userId, name, passcode, number, no_experiance)
             mycursor.execute(sql, val)
             mydb.commit()
-            return jsonify({"data": str(mycursor.rowcount) + 'record inserted'}), 
+            return jsonify({"data": str(mycursor.rowcount) + "record inserted"})
 
 
-     
 api.add_resource(Hello, "/hello")
-api.add_resource(student,"/student")
-
+api.add_resource(student, "/student")
 
 
 docs.register(Hello)
