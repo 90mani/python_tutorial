222d9dcf895cbe81e7c92016966ce73e86fe5cb9
90mani <manikandan.c@sankitsoiutions.com>
2023-08-22 21:18:55 +0530

    python

---
+++ python C1.py
@@ -0,0 +1,50 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========================== RESTART: F:/python C1.py ===========================
+HELLO PYTHON
+hello python
+Traceback (most recent call last):
+  File "F:/python C1.py", line 4, in <module>
+    print(Name.capilalize("LL"))
+AttributeError: 'str' object has no attribute 'capilalize'. Did you mean: 'capitalize'?
+
+=========================== RESTART: F:/python C1.py ===========================
+HELLO PYTHON
+hello python
+Hello python
+0
+1
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+False
+True
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+True
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+Minor
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+True
+Traceback (most recent call last):
+  File "F:/python C1.py", line 2, in <module>
+    print(ord("a"),ord(">="))
+TypeError: ord() expected a character, but string of length 2 found
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+True
+Traceback (most recent call last):
+  File "F:/python C1.py", line 2, in <module>
+    print(ord("50"),ord("20"))
+TypeError: ord() expected a character, but string of length 2 found
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+True
+True
+>>> 
+=========================== RESTART: F:/python C1.py ===========================
+False
+True
+False
---
+++ pythone.progrem1.py
@@ -0,0 +1,44 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: F:/pythone.progrem1.py =============
+40
+pass
+>>> 30
+30
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+30
+fail
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+income:7000
+eligible for scholarship
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+income:6500
+eligible for scholarship
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+15
+divisible by 3
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+14
+divisible by 3
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+15
+divisible by 3
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+15
+divisible by 3
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+12
+divisible by 3
+>>> 
+============= RESTART: F:/pythone.progrem1.py =============
+12
+  not divisible by 3 and 5
