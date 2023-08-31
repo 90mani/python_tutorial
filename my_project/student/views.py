5e25bff6105f8317aeb80b1713e594f2a9a47306
90mani <manikandan.c@sankitsolutions.com>
2023-08-31 10:25:57 +0530

    django program

--- my_project/student/views.py
+++ my_project/student/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
     return HttpResponse("This is django program!")
+def index(request):
+    template = loader.get_template('sample.html')
+    return HttpResponse(template.render())
+    
 
 
