8bbbeab68b7b941cd1359c2e32a39aa5de954652
Bhuvana <bhuvana56014@gmail.com>
2023-11-02 12:40:05 +0530

    sql quary

---
+++ tutorial/RESTapi1/sqldb/min1.sql
@@ -0,0 +1,13 @@
+#SELECT MIN(course)
+#FROM student;
+#SELECT MAX(course)
+#From student;
+#SELECT MIN(course) AS smallestprice FROM student;
+#SELECT COUNT(*) FROM student;
+#SELECT COUNT(name) FROM student;
+#SELECT COUNT(id) FROM student WHERE id >6; 
+#SELECT *FROM student;
+#UPDATE student
+#SET  NAME = NULL WHERE  name = 'baviya';
+#SELECT COUNT(distinct course) FROM student;
+
